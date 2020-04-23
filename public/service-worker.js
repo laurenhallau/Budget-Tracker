85514f309b56e5af6bfc515785ac90320f09fd3f
Lauren Hallau <lhallau@gmail.com>
2020-04-23 11:56:33 -0500

    fixed uncaught error

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,9 +2,9 @@
 const FILES_TO_CACHE = [
     "/", 
     "/index.html", 
-    "index.js", 
-    "./db.js", 
-    "./styles.css"];
+    "/index.js", 
+    "/db.js",
+    "/style.css"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
