7d1e11efe56ee6c81d465f42c3e90e3d514c3dac
Lauren Hallau <lhallau@gmail.com>
2020-04-23 12:25:37 -0500

    adding mongodb connection information

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connecting to the MongoDB or localDB
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
