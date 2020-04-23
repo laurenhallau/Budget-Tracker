5c73e1a80ba879733844b559cd36e3b0a1b9cfc6
Lauren Hallau <lhallau@gmail.com>
2020-04-23 11:42:54 -0500

    service worker running

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,4 @@
+// imported from 18-PWA, activity 13
 const FILES_TO_CACHE = [
     "/", 
     "/index.html", 
@@ -38,35 +39,35 @@ self.addEventListener("activate", function (evt) {
   self.clients.claim();
 });
 
-// fetch
-self.addEventListener("fetch", (evt) => {
-  if (evt.request.url.includes("/api/")) {
-    console.log("[Service Worker] Fetch(data)", evt.request.url);
+//fetch
+self.addEventListener("fetch", function (evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
 
     evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then((cache) => {
-        return fetch(evt.request)
-          .then((response) => {
-              // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
-            }
-            return response;
-          })
-          .catch((err) => {
-         // Network request failed, try to get it from the cache.
-            return cache.match(evt.request);
+        caches.open(CACHE_NAME).then(cache => {
+          return cache.match(evt.request).then(response => {
+            return response || fetch(evt.request);
           });
-      }).catch(err => console.log(err))
-    );
-    return;
-  }
-
-  evt.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(evt.request).then((response) => {
-        return response || fetch(evt.request);
-      });
-    })
-  );
-});
+        })
+      );
+    });
